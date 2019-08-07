67d348dacd29156b2ec65ef9fecf78f64cd56bb5
Eva Strunz <Eva@Evas-MacBook.fritz.box>
2019-08-07 19:46:33 +0200

    zutaten as view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RezeptComponent } from './rezept/rezept.component';
 import { StartComponent } from './start/start.component';
+import { ZutatenComponent } from './zutaten/zutaten.component';
 
 const routes: Routes = [
   { path: '', component: StartComponent},
+  { path: 'start', component: StartComponent},
   { path: 'rezept', component: RezeptComponent},
-  { path: 'rezept/:id', component:RezeptComponent }
+  { path: 'rezept/:id', component:RezeptComponent},
+  { path: 'zutaten', component: ZutatenComponent}
 ];
 
 @NgModule({
